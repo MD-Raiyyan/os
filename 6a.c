7512a0dd2473674ddba5d156ec6567f4d91de507
Mohammed Raiyyan Bepari <mohammedraiyyanbepari@gmail.com>
2026-06-12 00:27:08 +0530

    Create 6a.c

---
+++ 6a.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+struct message
+{
+    long type;
+    char text[100];
+};
+
+int main()
+{
+    int msgid;
+    struct message msg;
+
+    msgid = msgget(1234, 0666 | IPC_CREAT);
+
+    if (fork() == 0)
+    {
+        // Consumer
+        msgrcv(msgid, &msg, sizeof(msg.text), 1, 0);
+        printf("Consumer received: %s\n", msg.text);
+    }
+    else
+    {
+        // Producer
+        msg.type = 1;
+        strcpy(msg.text, "Hello from Producer");
+
+        msgsnd(msgid, &msg, sizeof(msg.text), 0);
+
+        printf("Producer sent: %s\n", msg.text);
+
+        wait(NULL);
+
+        msgctl(msgid, IPC_RMID, NULL);
+    }
+
+    return 0;
+}
